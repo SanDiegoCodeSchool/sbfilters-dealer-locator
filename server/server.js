510ce9fdee7fb44e4181a9a867df3db92798d0c6
hankfrank <henryfrank1@yahoo.com>
2020-09-29 15:42:19 -0700

    pulling data from FTP, comparing to data in DB and updating any new values.

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 // installed modules
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
+// const path = require('path');
 const fileUpload = require('express-fileupload');
-const axios = require('axios');
 const dotenv = require('dotenv').config();
+const cron = require('node-cron');
+// const ftp = require("basic-ftp");
+// const fs = require('fs');
+// f = require('util').format;
+// const tunnel = require('tunnel-ssh');
 
-// const crypto = require('crypto');
-// const testData = require('../miva-post-raw.json');
-
-// imported from mailer.js
-const mailer = require("./mailer")
+const updateDealers = require('./updateDealers');
+const dealerJson = require('../savedFiles/locations.json');
+// const testData = require('../savedFiles/newLocations.json');
 
 // create the express server
 const app = express();
@@ -23,95 +25,17 @@ app.use(bodyParser.json());
 // route handlers
 app.get('/',function(req,res){
     console.log('someone is trying to get the root...');
-    res.status(200).sendFile(path.join(__dirname + '/../testForm.html'));
+    res.status(200)
 });
 
-app.post('/register',function(req,res){
-    mailer(req);
-    res.redirect(301, 'https://www.sbfilters.com/new-dealer?submitted=true')
+app.get('/mongo', async (req , res) => {
+    updateDealers();
 });
 
-app.post('/miva', async (req, res) => {
-    console.log('in the miva route... ');
-    const {id, bill_email, bill_fname, bill_lname, bill_addr1, bill_city, bill_state, bill_zip, bill_cntry, formatted_total, orderdate, items} = req.body;
-    // const {id, bill_email, bill_fname, bill_lname, bill_addr1, bill_city, bill_state, bill_zip, bill_cntry, formatted_total, orderdate, items} = testData;
+cron.schedule('59 23 * * *', () => {
+    console.log("running at 11:59pm");
+    updateDealers();
+})
 
-    const imgMap = {
-        '10-1000': 'graphics/00000001/10-1000- 56 gallon capacity.jpg',
-        '100EE00AA2': 'graphics/00000001/EZ Lynk Auto Agent 2.0.jpg',
-        '10-1001': 'graphics/00000001/1001-V2.png',
-        '10-1002': 'graphics/00000001/10-1002 pictures.jpg',
-        '10-1004': 'graphics/00000001/10-1004 with single infographic.jpg',
-        '10-1005': 'graphics/00000001/MAIN10-1005- 14 Single Infographic.jpg',
-        '10-1006': 'graphics/00000001/10-1006- Single Infographic 7-19-19.jpg',
-        '10-1008': 'graphics/00000001/10-1008 thumbnail.jpg',
-        '10-2000': 'graphics/00000001/Tankcomp12 (1) (1).jpg',
-        '10-2001': 'graphics/00000001/Tankcomp12 (1) (1).jpg',
-    };
-    
-    const formattedItems = items.map((item) => {
-        const {sku, name, price} = item;
-        let nameCleanedforURL = name.replace(/[, ]+/g, "-").replace(/[.]+/g, "").trim().toLowerCase();
-
-        const eachItemObj = {
-            "productId": sku,
-            "productBrand": "SB Tanks",
-            "productDescription": name,
-            "productTitle": name,
-            "productImageUrl": encodeURI(`http://sbtanks.com/mm5/${imgMap[sku]}`),
-            "productPrice": `$${price}`,
-            "productType": "",
-            //this will need to be swapped for the live site
-            "productUrl": `http://dev.sbtanks.com/${nameCleanedforURL}`
-        }
-        return eachItemObj;
-    })
-
-    const stampedSettings = {
-        "url": `https://stamped.io/api/v2/${process.env.STAMPED_STORE_HASH}/survey/reviews/bulk`,
-        "method": "POST",
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        "auth": {
-            "username": process.env.STAMPED_PUBLIC_KEY,
-            "password": process.env.STAMPED_PRIVATE_KEY
-        },
-        "data": JSON.stringify(
-            [
-                {
-                    "email": bill_email,
-                    "firstName": bill_fname,
-                    "lastName": bill_lname,
-                    "location": `${bill_addr1} ${bill_city} ${bill_state}, ${bill_zip} ${bill_cntry}`,
-                    "orderNumber": id,
-                    "orderId": id,
-                    "orderCurrencyISO": "USD",
-                    "orderTotalPrice": formatted_total,
-                    "orderSource": "SB Tanks",
-                    "orderDate": orderdate,
-                    "dateScheduled": orderdate,
-                    "itemsList": formattedItems
-                }
-            ]
-        ),
-    };
-
-    try {
-        let stampedResult = await axios(stampedSettings);
-
-        if (stampedResult) {
-            if (stampedResult.data) {
-                console.log(`Stamped Result data: `, stampedResult.data);
-                if (stampedResult.data.length == 0) {
-                    console.log('Successful stamped post, unsuccessful email generation, most likely a duplicate order number.')
-                }
-            }
-
-        }
-    } catch (err) {
-        console.log(`Error with stmaped post: ${err}`);
-    }
-});
 
 module.exports = app;
---
+++ server/updateDealers.js
@@ -0,0 +1,136 @@
+const ftp = require("basic-ftp");
+const dotenv = require('dotenv').config();
+
+const dealerJson = require('../savedFiles/locations.json');
+// const testData = require('../savedFiles/newLocations.json');
+
+const updateDealers = async () => {
+    //Specify the Amazon DocumentDB cert
+    // const ca = [fs.readFileSync(path.join(__dirname + '/../savedFiles/rds-combined-ca-bundle.pem'), 'utf8')];
+    // const MongoClient = require('mongodb').MongoClient;
+    // const url = `mongodb://${process.env.DOCUEMNT_USER}:${process.env.DOCUMENT_PASSWORD}@docdb-2020-09-28-20-35-00.cluster-crkcmdiwbs7h.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false`;
+    // const client = new MongoClient(url, { 
+    //     sslValidate: true,
+    //     sslCA: ca,
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true
+    // }); 
+
+    const MongoClient = require('mongodb').MongoClient;
+    const assert = require('assert');
+    const url = 'mongodb://localhost:27017';
+    const dbName = 'sbdealers';
+    
+    const client = new MongoClient(url, { useUnifiedTopology: true });  
+
+    const insertData = () => {
+        client.connect((err, client) => {
+            if (err) console.log(`Insert Data Connect Error: `, err);
+            console.log("Connected to server insert");
+            const db = client.db(dbName);
+            const col = db.collection('dealers');
+
+            col.insertOne({name: "dealerJson", data: dealerJson}, (err, res) => {
+                if (err) console.log("Insert Error: ", err);
+                console.log("inserted document: ", res);
+            })
+        });   
+    };
+    
+
+    const getData = (mivaData) => {
+        client.connect((err, client) => {
+            if (err) console.log(`Get data Connect Error: `, err);
+            console.log("Connected to DB to Get Data");
+            const db = client.db(dbName);
+            const col = db.collection('dealers');
+
+            col.findOne({name: "dealerJson"}, (err, res) => {
+                if (err) console.log(`Find Error: `, err);
+                if (res == null) {
+                    // insertData();
+                    const allDealers = compareData(dealerJson, mivaData);
+                    if (allDealers.length > dealerJson.length) {
+                        const howMany = allDealers.length - dealerJson.length;
+                        updateData(allDealers, howMany);
+                    } else {
+                        console.log("No new dealers to update this round.")
+                    }
+                } else {
+                    const dbDealers = res.data;
+                    const allDealers = compareData(dbDealers, mivaData);
+                    if (allDealers.length > dbDealers.length) {
+                        const howMany = allDealers.length - dbDealers.length;
+                        updateData(allDealers, howMany);
+                    } else {
+                        console.log("No new dealers to update this round.")
+                    }
+                }
+            })
+        });
+    };
+
+    const compareData = (dbData, newData) => {
+        let newDealers = newData.filter((obj) => {
+            return !dbData.some((obj2) => {
+                return obj.id == obj2.id;
+            })
+        });
+        
+        if (newDealers.length > 0) {
+            return [...dbData, ...newDealers];
+        } else {
+            return dbData;
+        }
+    };
+
+    const updateData = (updatedData, howMany) => {
+        client.connect((err, client) => {
+            if (err) console.log(`Update connection Error: `, err);
+            console.log("Connected to server update");
+            const db = client.db(dbName);
+            const col = db.collection('dealers');
+            const query = {name: "dealerJson"};
+            const newData = {$set: {data: updatedData}};
+
+            col.updateOne(query, newData, (err, res) => {
+                if (err) {console.log(`Update error: `, err)};
+                if (res.modifiedCount == 1) {
+                    console.log(`Updated the DB with ${howMany} new Dealers.`);
+                }
+            });
+        });
+    }
+    
+    const mivaFtpCredentials = {
+        host: process.env.MIVA_HOST,
+        user: process.env.MIVA_USER,
+        password: process.env.MIVA_PASSWORD,
+    };
+
+    const mivaClient = new ftp.Client();
+    mivaClient.ftp.verbose = true;
+    const filePathInMiva = `mm5/themes/shadows/custom-styles/testData/newLocations.json`;
+    // const filePathInMiva2 = `mm5/themes/shadows/custom-styles/newLocations.json`;
+    const filePathToLocalFile = `${__dirname}/newLocations.json`;
+
+    try {
+        await mivaClient.access(mivaFtpCredentials);
+        mivaClient.trackProgress(info => {
+            console.log("File", info.name)
+            console.log("Type", info.type)
+            console.log("Transferred", info.bytes)
+            console.log("Transferred Overall", info.bytesOverall)
+        })
+        console.log(`List: `, await mivaClient.list(`mm5/themes/shadows/custom-styles/testData`));
+        await mivaClient.downloadTo(filePathToLocalFile, filePathInMiva);
+        const mivaData = require(filePathToLocalFile);
+        console.log(`Miva Data acquired, updating DB.`);
+        getData(mivaData);
+        // mivaClient.close();
+    } catch(err) {
+        console.log(`Miva Error: `, err);
+    }
+};
+
+module.exports = updateDealers;
